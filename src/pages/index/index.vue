6922585ec18d314fa5f5073d663cfc0aaf751774
WeiSheng <842469165@qq.com>
2024-01-30 19:38:01 +0800

    update:安装uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,31 @@
+<!--
+ * @Author: 微生
+ * @Date: 2024-01-30 15:39:17
+ * @LastEditors: WeiSheng 842469165@qq.com
+ * @LastEditTime: 2024-01-30 19:34:40
+ * @FilePath: /demo-xtx/src/pages/index/index.vue
+ * @Description:
+ *
+ * Copyright (c) 2024 by 微生, All Rights Reserved.
+-->
+<template>
+  <view class="index">
+    <uni-card
+      title="基础卡片"
+      sub-title="副标题"
+      extra="额外信息"
+      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+    >
+      <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+    </uni-card>
+    <uni-card title="基础卡片" extra="额外信息">
+      <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+    </uni-card>
+  </view>
+</template>
 <script setup lang="ts">
 //
 </script>
-
-<template>
-  <view class="index">index</view>
-</template>
 
 <style lang="scss">
 //
